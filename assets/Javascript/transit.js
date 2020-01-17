1e1b2ae39b015ce5c362a606b893420c1c25b5ec
AJ <ascott1083@yahoo.com>
2020-01-17 15:42:59 -0600

    typose

--- assets/Javascript/transit.js
+++ assets/Javascript/transit.js
@@ -1,14 +1,4 @@
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDzKI_92qiuHScO4sErc4__FsO988zVU1g",
-//   authDomain: "trainschedule-1bb80.firebaseapp.com",
-//   databaseURL: "https://trainschedule-1bb80.firebaseio.com",
-//   projectId: "trainschedule-1bb80",
-//   storageBucket: "trainschedule-1bb80.appspot.com",
-//   messagingSenderId: "648525075307",
-//   appId: "1:648525075307:web:e3b78eaec62850de63f22e",
-//   measurementId: "G-N2EG7WED90"
-// };
-
+// initialize Firebase
 var config = {
   apiKey: "AIzaSyC1-lfMm-ICxtXmyZYMaB8kS4it_jaZODI",
   authDomain: "transit-schedules.firebaseapp.com",
